e71b1a4b4006701b5dd4008ad3ccc278e2c254c4
Master4020 <47930042+Master4020@users.noreply.github.com>
2020-12-21 19:25:53 -0500

    I keep corruting file and footer finished

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,38 +1,63 @@
-import React, {usestate} from 'react';
+import React, { usestate } from "react";
+import github from "./images/github.png";
+import gmail from "./images/gmail.webp";
+import discord from "./images/discord.png";
+import reddit from "./images/reddit.webp";
 
-function footer(){
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-    const items = [
-        {
-            name: "name1",
-            image: 'img',
-            link: "#"
-        },
-        {
-            name: "name1",
-            image: 'img',
-            link: "#"
-        },
-        
-        {
-            name: "name1",
-            image: 'img',
-            link: "#"
-        }
-    ]
+AOS.init({
+  duration: 1500,
+});
 
-    // change to image later
-    const listItems = items.map((items) => <li><a href={items.link}>{items.name}</a></li>)
+function footer() {
+  const contacts = [
+    {
+      name: "email",
+      image: gmail,
+      link: "mailto:christienyenwalker@gmail.com",
+    },
+    {
+      name: "github",
+      image: github,
+      link: "https://github.com/ChrisTeinYenWalker",
+    },
 
-    return(
-        <div>
-            <nav>
-                <ul>
-                    <ul>{listItems}</ul>
-                </ul>
-            </nav>
-        </div>
-    )
+    {
+      name: "reddit",
+      image: reddit,
+      link: "https://www.reddit.com/user/master4020/",
+    },
+
+    {
+      name: "discord",
+      image: discord,
+      link: "https://discord.com/users/master#3162",
+    },
+  ];
+
+  // change to image later
+  const contactlist = contacts.map((contact) => (
+    <li>
+      <a href={contact.link}>
+        <img src={contact.image}></img>
+      </a>
+    </li>
+  ));
+
+  return (
+    <div className="footer">
+      <div data-aos="zoom-in-up">
+        <p>My Contacts</p>
+        <nav>
+          <ul>
+            <ul>{contactlist}</ul>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default footer;
--- src/website.js
+++ src/website.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Header from './components/header';
-import Body from './components/body';
-import Footer from './components/footer';
-import './styles.css'
+import React from "react";
+import Header from "./components/header";
+import Body from "./components/body";
+import Footer from "./components/footer";
+import "./styles.css";
 
 function website() {
-    return (
-        <div>
-            <Header></Header>
-             <Body></Body> 
-            {/*<Footer></Footer>*/}
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default website;
